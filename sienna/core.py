9314aa86a214de7d92db2ba4128253e4da04ae1f
sobamchan <oh.sore.sore.soutarou@gmail.com>
2022-05-03 14:50:48 +0200

    support json, jsonl, txt

--- sienna/core.py
+++ sienna/core.py
@@ -2,12 +2,37 @@ import json
 from typing import Dict, List, Union
 
 
+def __get_file_type(fpath: str) -> str:
+    if fpath.endswith(".jsonl"):
+        return "jsonl"
+    elif fpath.endswith(".json"):
+        return "jsonl"
+    else:
+        return "text"
+
+
 def load(fpath: str) -> Union[List, Dict]:
-    with open(fpath, "r") as f:
-        data = [json.loads(line) for line in f.readlines()]
+    ftype = __get_file_type(fpath)
+    if ftype == "jsonl":
+        with open(fpath, "r") as f:
+            data = [json.loads(line) for line in f.readlines()]
+    elif ftype == "json":
+        with open(fpath, "r") as f:
+            data = json.load(f)
+    else:
+        with open(fpath, "r") as f:
+            data = [line.strip() for line in f.readlines()]
     return data
 
 
 def save(data: Union[List, Dict], fpath: str) -> None:
-    with open(fpath, "w") as f:
-        f.write("\n".join([json.dumps(d) for d in data]))
+    ftype = __get_file_type(fpath)
+    if ftype == "jsonl":
+        with open(fpath, "w") as f:
+            f.write("\n".join([json.dumps(d) for d in data]))
+    elif ftype == "json":
+        with open(fpath, "w") as f:
+            json.dump(data, f)
+    else:
+        with open(fpath, "w") as f:
+            f.write("\n".join(data))
